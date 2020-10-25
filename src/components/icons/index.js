971a1dfc87cf467ad501df388bfa28d0ca320c89
abdulhay103 <mdabdulhay95@gmail.com>
2020-10-25 23:58:33 +0600

    About Us & Contact Us Eddid

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -2,6 +2,6 @@ import React from "react";
 import { Icon } from "./styles/icons";
 
 export default function Icons({ className }) {
-  console.log(className);
+  // console.log(className);
   return <Icon className={className} />;
 }
